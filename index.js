ec764ac15821cad5e12d006ec961e757e0af1785
Himanshu-Mendhe <himanshu.afcwp23@sinhgad.edu>
2024-06-08 20:08:38 +0530

    ADD:added functionality of restart and trying to apply restart button after the movie info is provided

--- index.js
+++ index.js
@@ -25,36 +25,41 @@ const fetchMovie = async (title, year) => {
     }
 };
 
-bot.start((ctx) => {
-    ctx.reply('Welcome to the bot', {
+const startCommand = (ctx) => {
+    ctx.reply('Welcome to the bot, what do you want to do', {
         reply_markup: {
             inline_keyboard: [
                 [
-                    { text: 'Movies', callback_data: 'movies' },
-                    { text: 'Series', callback_data: 'series' }
+                    { text: 'Information about Movies', callback_data: 'movies' },
+                    { text: 'restart', callback_data: 'restart' }
                 ]
             ]
         },
         parse_mode: "Markdown",
         disable_web_page_preview: true
     });
-});
+};
 
-bot.on('callback_query', (ctx) => {
-    const callbackData = ctx.callbackQuery.data;
+bot.start((ctx)=>{
+    startCommand(ctx)
+})
 
-    if (callbackData === 'movies') {
-        ctx.reply('You clicked Movies. Please enter the name of the movie:');
-        
-        bot.on('text', async (ctx) => {
-            const userInput = ctx.message.text;
-            try {
-                const movieData = await fetchMovie(userInput);
 
-                if (movieData.Response === 'False') {
-                    ctx.reply(`Sorry, I couldn't find any movie with the title "${userInput}".`);
-                } else {
-                    const replyMessage = `
+bot.action('restart',(ctx)=>{
+    ctx.reply("restarting the bot")
+    startCommand(ctx)
+})
+
+const reply = () => {
+    bot.on('text', async (ctx) => {
+        const userInput = ctx.message.text;
+        try {
+            const movieData = await fetchMovie(userInput);
+
+            if (movieData.Response === 'False') {
+                ctx.reply(`Sorry, I couldn't find any movie with the title "${userInput}".`);
+            } else {
+                const replyMessage = `
 *Title:* ${movieData.Title}
 *Year:* ${movieData.Year}
 *Rated:* ${movieData.Rated}
@@ -70,15 +75,26 @@ bot.on('callback_query', (ctx) => {
 *Awards:* ${movieData.Awards}
 *IMDB Rating:* ${movieData.imdbRating}
 `;
-                    ctx.reply(replyMessage);
-                }
-            } catch (error) {
-                ctx.reply('An error occurred while fetching the movie data. Please try again later.');
+                ctx.reply(replyMessage);
             }
-        });
-    } else if (callbackData === 'series') {
-        ctx.reply('You clicked Series');
-    } else {
+
+            
+            
+        } catch (error) {
+            ctx.reply('An error occurred while fetching the movie data. Please try again later.');
+        }
+    });
+}
+
+
+bot.on('callback_query', (ctx) => {
+    const callbackData = ctx.callbackQuery.data;
+
+    if (callbackData === 'movies') {
+        ctx.reply('You clicked Movies. Please enter the name of the movie:');
+        reply();
+    }
+    else {
         ctx.reply('Unknown option');
     }
 });
